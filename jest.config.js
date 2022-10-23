b35e61e030f8b176a5d53ab0131959512379b4d7
Bruno Carmo Nunes <brunocmo.nunes@gmail.com>
2022-10-23 19:13:45 -0300

    Add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+
+module.exports = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  moduleFileExtensions: [
+    'js',
+    'mjs',
+    'cjs',
+    'jsx',
+    'ts',
+    'tsx',
+    'json',
+    'node',
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**'],
+  coveragePathIgnorePatterns: [
+    '/src/index.js',
+  ],
+  reporters: [
+    'default',
+    [
+      'jest-sonar',
+      {
+        outputDirectory: 'coverage/',
+        outputName: 'jest-report.xml',
+        reportedFilePath: 'relative',
+        relativeRootDir: '<rootDir>/../',
+      },
+    ],
+  ],
+  testMatch: [
+    '**/*.test.js',
+  ],
+};
